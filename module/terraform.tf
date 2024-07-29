46992668cbaae7261c28a1621590d0ac4e5947d6
saidben0 <saidben7243@gmail.com>
2024-07-29 14:30:28 -0400

    Update terraform.tf

--- module/terraform.tf
+++ module/terraform.tf
@@ -1,6 +1,5 @@
 terraform {
-  # required_version = "~> 1.9.3"
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.9.2"
   required_providers {
     aws = {
       source                = "hashicorp/aws"
